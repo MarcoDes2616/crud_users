4ff6d10f71c641c4684d2cd696db557960242b71
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-01-13 19:06:58 -0500

    first commit

---
+++ src/components/UserList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+const UserList = ({ usersList, selectUser, getForm, warning }) => {
+    
+    const orderedList = usersList.sort((a, b) => a.first_name.localeCompare(b.first_name));
+
+    return (
+        <div className='list__users'>
+            <h2>Lista de Usuarios</h2>
+            <div className='sumary__list'>
+                <p><strong>Usuarios Existentes: </strong>{usersList.length}</p>
+                <button onClick={() => getForm()}>+ Crear nuevo usuario</button>
+            </div>
+            <div className='container__card'>
+            {orderedList.map((user) => (
+                <div key={user.id} className="card__user">
+                    <h3>{user.first_name}, {user.last_name}</h3>
+                    <div className='body__card'>
+                        <div className='info__user'>
+                            <p><b>E-mail: </b>{user.email}</p>
+                            <p><b>Birthday: </b>{user.birthday}</p>
+                        </div>
+                        <div className='user__controler'>
+                            <div className='btn__edit' onClick={() => selectUser(user)}><i className='bx bxs-edit-alt bx-sm'></i></div>
+                            <div className='btn__warning' onClick={() => warning(user)}><i className='bx bx-message-square-x bx-sm'></i></div>
+                        </div>
+                    </div>
+                </div>
+            ))
+            }
+            </div>
+        </div>
+    );
+};
+
+export default UserList;
